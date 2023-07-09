43e33ee50503a10088f18240f528d3b4698d41ff
Felipe Petriw <felipe.petriw.ads@gmail.com>
2023-07-09 19:00:06 -0300

    Finalização Projeto
    Finalização de Scripts

--- scripts.js
+++ scripts.js
@@ -2,12 +2,42 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
         
         //Remove o realce de contorno do elemento
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
+        removerSelecaoDoPersonagem();
         
         //Adiciona o realce no contorno do elemento
         personagem.classList.add('selecionado');
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagemSelecionado(personagem);
     })
 })
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerHTML = personagem.getAttribute('name');
+}
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerHTML = personagem.getAttribute('description');
+}
